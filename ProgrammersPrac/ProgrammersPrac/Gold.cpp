aa7e205202f2363371f013583b7c777e84cd161a
MyeongjinMoon <114853338+MyeongjinMoon@users.noreply.github.com>
2025-05-01 19:44:05 +0900

    feat:  추가

--- ProgrammersPrac/ProgrammersPrac/Gold.cpp
+++ ProgrammersPrac/ProgrammersPrac/Gold.cpp
@@ -3,14 +3,14 @@
 void RestOfSum();
 void GoodNumber();
 
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	//RestOfSum();
-	GoodNumber();
-}
+//int main()
+//{
+//	ios::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	//RestOfSum();
+//	//GoodNumber();
+//}
 
 // ----- RestOfSum -----
 void RestOfSum()
--- ProgrammersPrac/ProgrammersPrac/Silver.cpp
+++ ProgrammersPrac/ProgrammersPrac/Silver.cpp
@@ -6,19 +6,21 @@ void SumOfSection();
 void SumOfSection2();
 void SumConsecutiveNumber();
 void OrderOfJumong();
+void DNA_Password();
 
-//int main()
-//{
-//	ios::sync_with_stdio(false);
-//	cin.tie(NULL); cout.tie(NULL);
-//
-//	//SumOfNumbers();
-//	//CalculateAverage();
-//	//SumOfSection();
-//	//SumOfSection2();
-//	//SumConsecutiveNumber();
-//	//OrderOfJumong();
-//}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	//SumOfNumbers();
+	//CalculateAverage();
+	//SumOfSection();
+	//SumOfSection2();
+	//SumConsecutiveNumber();
+	//OrderOfJumong();
+	DNA_Password();
+}
 
 // ----- SumOfNumbers -----
 void SumOfNumbers()
@@ -193,4 +195,47 @@ void OrderOfJumong()
 	}
 
 	cout << answer;
+}
+
+// ----- DNA_Password -----
+bool IsInDNA(char password, int& index);
+void DNA_Password()
+{
+	string dna_Str;
+	int* dna = (int*)calloc(sizeof(int), 4);
+	int* _dna = (int*)calloc(sizeof(int), 4);
+	int s, p;
+	cin >> s >> p >> dna_Str;
+
+	for (int i = 0; i < 4; i++)
+		cin >> dna[i];
+
+	for (int i = 0; i < p; i++)
+	{
+		int index = 0;
+		if (IsInDNA(dna_Str[i], index))
+		{
+			_dna[index]++;
+		}
+	}
+}
+bool IsInDNA(char password, int& index)
+{
+	switch (password)
+	{
+	case 'A':
+		index = 0;
+		return true;
+	case 'C':
+		index = 1;
+		return true;
+	case 'G':
+		index = 2;
+		return true;
+	case 'T':
+		index = 3;
+		return true;
+	default:
+		return false;
+	}
 }
